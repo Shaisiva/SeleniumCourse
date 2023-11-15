eab5bb694db6da0b4ae28a2cb4d6852e548f1633
Shaisiva <32162906+Shaisiva@users.noreply.github.com>
2023-11-15 10:10:03 +0530

    pangram.java
    Pangram means - string should contain a to z character at least once in the sentence.

---
+++ src/Interview/pangram.java
@@ -0,0 +1,38 @@
+// A Java Program to check if the given
+// string is a pangram or not
+import java.util.*;
+
+public class pangram {
+
+	// Returns true if the string is pangram else false
+	static boolean checkPangram(String str) {
+		// Initialize a set of characters
+		Set<Character> set = new HashSet<>();
+
+		for (char ch : str.toCharArray()) {
+			// If the character is already
+			// a lowercase character
+			if (ch >= 'a' && ch <= 'z')
+				set.add(ch);
+
+			// In case of a uppercase character
+			if (ch >= 'A' && ch <= 'Z') {
+				// convert to lowercase
+				ch = Character.toLowerCase(ch);
+				set.add(ch);
+			}
+		}
+
+		// check if the size is 26 or not
+		return set.size() == 26;
+	}
+
+	// Driver Program to test above functions
+	public static void main(String[] args) {
+		String str = "The quick brown fox jumps over the lazy dog";
+		if (checkPangram(str))
+			System.out.println("It is a Pangram");
+		else
+			System.out.println("It is Not a Pangram");
+	}
+}
